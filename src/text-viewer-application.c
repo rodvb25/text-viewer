9fd078cf68128e5028bd020d418e85c2d3db17dc
rodrigo <rodrigovbartholo@gmail.com>
2024-03-11 12:06:06 -0300

    Load content from a file - With Error

--- src/text-viewer-application.c
+++ src/text-viewer-application.c
@@ -23,98 +23,78 @@
 #include "text-viewer-application.h"
 #include "text-viewer-window.h"
 
-struct _TextViewerApplication
-{
-	AdwApplication parent_instance;
+struct _TextViewerApplication {
+  AdwApplication parent_instance;
 };
 
-G_DEFINE_TYPE (TextViewerApplication, text_viewer_application, ADW_TYPE_APPLICATION)
+G_DEFINE_TYPE(TextViewerApplication, text_viewer_application,
+              ADW_TYPE_APPLICATION)
 
-TextViewerApplication *
-text_viewer_application_new (const char        *application_id,
-                             GApplicationFlags  flags)
-{
-	g_return_val_if_fail (application_id != NULL, NULL);
+TextViewerApplication *text_viewer_application_new(const char *application_id,
+                                                   GApplicationFlags flags) {
+  g_return_val_if_fail(application_id != NULL, NULL);
 
-	return g_object_new (TEXT_VIEWER_TYPE_APPLICATION,
-	                     "application-id", application_id,
-	                     "flags", flags,
-	                     NULL);
+  return g_object_new(TEXT_VIEWER_TYPE_APPLICATION, "application-id",
+                      application_id, "flags", flags, NULL);
+}
+
+static void text_viewer_application_activate(GApplication *app) {
+  GtkWindow *window;
+
+  g_assert(TEXT_VIEWER_IS_APPLICATION(app));
+
+  window = gtk_application_get_active_window(GTK_APPLICATION(app));
+
+  if (window == NULL)
+    window = g_object_new(TEXT_VIEWER_TYPE_WINDOW, "application", app, NULL);
+
+  gtk_window_present(window);
 }
 
 static void
-text_viewer_application_activate (GApplication *app)
-{
-	GtkWindow *window;
+text_viewer_application_class_init(TextViewerApplicationClass *klass) {
+  GApplicationClass *app_class = G_APPLICATION_CLASS(klass);
 
-	g_assert (TEXT_VIEWER_IS_APPLICATION (app));
-
-	window = gtk_application_get_active_window (GTK_APPLICATION (app));
-
-	if (window == NULL)
-		window = g_object_new (TEXT_VIEWER_TYPE_WINDOW,
-		                       "application", app,
-		                       NULL);
-
-	gtk_window_present (window);
+  app_class->activate = text_viewer_application_activate;
 }
 
-static void
-text_viewer_application_class_init (TextViewerApplicationClass *klass)
-{
-	GApplicationClass *app_class = G_APPLICATION_CLASS (klass);
+static void text_viewer_application_about_action(GSimpleAction *action,
+                                                 GVariant *parameter,
+                                                 gpointer user_data) {
+  static const char *developers[] = {"Unknown", NULL};
+  TextViewerApplication *self = user_data;
+  GtkWindow *window = NULL;
 
-	app_class->activate = text_viewer_application_activate;
+  g_assert(TEXT_VIEWER_IS_APPLICATION(self));
+
+  window = gtk_application_get_active_window(GTK_APPLICATION(self));
+
+  adw_show_about_window(
+      window, "application-name", "text-viewer", "application-icon",
+      "com.example.TextViewer", "developer-name", "Unknown", "version", "0.1.0",
+      "developers", developers, "copyright", "© 2024 Unknown", NULL);
 }
 
-static void
-text_viewer_application_about_action (GSimpleAction *action,
-                                      GVariant      *parameter,
-                                      gpointer       user_data)
-{
-	static const char *developers[] = {"Unknown", NULL};
-	TextViewerApplication *self = user_data;
-	GtkWindow *window = NULL;
+static void text_viewer_application_quit_action(GSimpleAction *action,
+                                                GVariant *parameter,
+                                                gpointer user_data) {
+  TextViewerApplication *self = user_data;
 
-	g_assert (TEXT_VIEWER_IS_APPLICATION (self));
+  g_assert(TEXT_VIEWER_IS_APPLICATION(self));
 
-	window = gtk_application_get_active_window (GTK_APPLICATION (self));
-
-	adw_show_about_window (window,
-	                       "application-name", "text-viewer",
-	                       "application-icon", "com.example.TextViewer",
-	                       "developer-name", "Unknown",
-	                       "version", "0.1.0",
-	                       "developers", developers,
-	                       "copyright", "© 2024 Unknown",
-	                       NULL);
-}
-
-static void
-text_viewer_application_quit_action (GSimpleAction *action,
-                                     GVariant      *parameter,
-                                     gpointer       user_data)
-{
-	TextViewerApplication *self = user_data;
-
-	g_assert (TEXT_VIEWER_IS_APPLICATION (self));
-
-	g_application_quit (G_APPLICATION (self));
+  g_application_quit(G_APPLICATION(self));
 }
 
 static const GActionEntry app_actions[] = {
-	{ "quit", text_viewer_application_quit_action },
-	{ "about", text_viewer_application_about_action },
+    {"quit", text_viewer_application_quit_action},
+    {"about", text_viewer_application_about_action},
 };
 
-static void
-text_viewer_application_init (TextViewerApplication *self)
-{
-	g_action_map_add_action_entries (G_ACTION_MAP (self),
-	                                 app_actions,
-	                                 G_N_ELEMENTS (app_actions),
-	                                 self);
-	gtk_application_set_accels_for_action (GTK_APPLICATION (self),
-	                                       "app.quit",
-	                                       (const char *[]) { "<primary>q", NULL });
+static void text_viewer_application_init(TextViewerApplication *self) {
+  g_action_map_add_action_entries(G_ACTION_MAP(self), app_actions,
+                                  G_N_ELEMENTS(app_actions), self);
+  gtk_application_set_accels_for_action(GTK_APPLICATION(self), "app.quit",
+                                        (const char *[]){"<primary>q", NULL});
+  gtk_application_set_accels_for_action(GTK_APPLICATION(self), "win.open",
+                                        (const char *[]){"<Ctrl>o", NULL});
 }
--- src/text-viewer-window.c
+++ src/text-viewer-window.c
@@ -19,6 +19,7 @@
  */
 
 #include "config.h"
+#include "glib.h"
 
 #include "text-viewer-window.h"
 
@@ -28,10 +29,91 @@ struct _TextViewerWindow {
   /* Template widgets */
   AdwHeaderBar *header_bar;
   GtkTextView *main_text_view;
+  GtkButton *open_button;
 };
 
 G_DEFINE_FINAL_TYPE(TextViewerWindow, text_viewer_window,
                     ADW_TYPE_APPLICATION_WINDOW)
+
+static void open_file_complete(GObject *source_object, GAsyncResult *result,
+                               TextViewerWindow *self) {
+  GFile *file = G_FILE(source_object);
+
+  g_autofree char *contents = NULL;
+  gsize length = 0;
+
+  g_autoptr(GError) error = NULL;
+
+  // Complete the asynchronous operation; this function will either
+  // give you the contents of the file as a byte array, or will
+  // set the error argument
+  g_file_load_contents_finish(file, result, &contents, &length, NULL, &error);
+  g_autofree char *display_name = NULL;
+  g_autoptr(GFileInfo) info = g_file_query_info(
+      file, "standard::display_name", G_FILE_QUERY_INFO_NONE, NULL, NULL);
+  if (info != NULL) {
+    display_name = g_strdup(
+        g_file_info_get_attribute_string(info, "standard::display_name"));
+  } else {
+    display_name = g_file_get_basename(file);
+  }
+  // in case of a error, print a warning to the standard error output
+  if (error != NULL) {
+    g_printerr("Unable to open “%s”: %s\n", g_file_peek_path(file),
+               error->message);
+    return;
+  }
+
+  // ensure that the file is encoded with UTF-8
+  if (!g_utf8_validate(contents, length, NULL)) {
+    g_printerr("Unable to load the contents of “%s”: "
+               "the file is not encoded with UTF-8\n",
+               g_file_peek_path(file));
+    return;
+  }
+
+  // retrieve the GtkTextBuffer instance that stores the text displayed by the
+  // GtkTextView widget
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer(self->main_text_view);
+
+  // set the text using the contents of the file
+  gtk_text_buffer_set_text(buffer, contents, length);
+
+  // reposition the cursor so it's at the start of the text
+  GtkTextIter start;
+  gtk_text_buffer_get_start_iter(buffer, &start);
+  gtk_text_buffer_place_cursor(buffer, &start);
+
+  // set the title using the display name
+  gtk_window_set_title(GTK_WINDOW(self), display_name);
+}
+
+static void open_file(TextViewerWindow *self, GFile *file) {
+  g_file_load_contents_async(file, NULL, (GAsyncReadyCallback)open_file_complete,
+                          self);
+}
+
+static void on_open_response(GObject *source, GAsyncResult *result,
+                             gpointer user_data) {
+  GtkFileDialog *dialog = GTK_FILE_DIALOG(source);
+  TextViewer *self = user_data;
+
+  g_autoptr(GFile) file = gtk_file_dialog_open_finish(dialog, result, NULL);
+
+  // if the user selected a file, open it
+  if (file != NULL) {
+    open_file(self, file);
+  }
+}
+
+static void
+text_viewer_window__open_file_dialog(GAction *action G_GNUC_UNUSED,
+                                     GVariant *parameter G_GNUC_UNUSED,
+                                     TextViewerWindow *self) {
+  g_autoptr(GtkFileDialog) dialog = gtk_file_dialog_new();
+
+  gtk_file_dialog_open(dialog, GTK_WINDOW(self), NULL, on_open_response, self);
+}
 
 static void text_viewer_window_class_init(TextViewerWindowClass *klass) {
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(klass);
@@ -42,8 +124,15 @@ static void text_viewer_window_class_init(TextViewerWindowClass *klass) {
                                        header_bar);
   gtk_widget_class_bind_template_child(widget_class, TextViewerWindow,
                                        main_text_view);
+  gtk_widget_class_bind_template_child(widget_class, TextViewerWindow,
+                                       open_button);
 }
 
 static void text_viewer_window_init(TextViewerWindow *self) {
   gtk_widget_init_template(GTK_WIDGET(self));
+
+  g_autoptr(GSimpleAction) open_action = g_simple_action_new("open", NULL);
+  g_signal_connect(open_action, "activate",
+                   G_CALLBACK(text_viewer_window__open_file_dialog), self);
+  g_action_map_add_action(G_ACTION_MAP(self), G_ACTION(open_action));
 }
